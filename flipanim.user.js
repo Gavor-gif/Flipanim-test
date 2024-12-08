12af09658b1d2aef95e61ee2b325d6ce6a876183
gavorgif <gamingwithgavin688@gmail.com>
2024-12-08 15:01:56 +0300

    Added initial userscript

---
+++ flipanim.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Flipanim Custom Script
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  A custom userscript for Flipanim
+// @author       You
+// @match        https://flipanim.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your custom code goes here
+    console.log("Flipanim Custom Script is running!");
+
+    // Example: Change the background color
+    document.body.style.backgroundColor = "lightblue";
+})();
